c052f05a87b2387f723afe000151ab1bfedb028f
mh-mehryar <44663511+mh-mehryar@users.noreply.github.com>
2021-11-08 13:10:25 +0330

    fibonacci < 20

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNum = new List<int> { 1, 1 };
+
+            while (fibonacciNum.Count < 20)
+            {
+                var previous = fibonacciNum[fibonacciNum.Count - 1];
+                var previous2 = fibonacciNum[fibonacciNum.Count - 2];
+
+                fibonacciNum.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNum)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
